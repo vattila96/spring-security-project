5b291d45a94786eaa0890f1db9bd8846f7ca93cb
Attila Varga <srep.ati@gmail.com>
2020-11-25 12:51:45 +0100

    update controller to login with bootstrap

--- src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+	
 	@GetMapping("/showMyLoginPage")
 	public String showMyLoginPage() {
-		return "plain-login";
+		return "fancy-login";
 	}
 }
