c9f9697923ff8ac0e299f687e1f2ea391892e6a0
Attila Varga <srep.ati@gmail.com>
2021-01-16 21:20:35 +0100

    update configs

--- src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -2,4 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {}
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
